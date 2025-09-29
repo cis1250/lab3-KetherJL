3260141bf97970797e90b57a6ec16ce14c79f00b
KetherJL <jlim18@uoguelph.ca>
2025-09-29 12:37:19 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python3
-
-# Word frequency exercise
-# TODO: (Read detailed instructions in the Readme file)
-# 1. Prompt the user: Ask the user to enter a sentence.
-# 2. Split the sentence
-# 3. Create lists to store words and their corresponding frequencies.
-# 4. Iterate through words and update frequencies
-
 import re
 
 #This is a function that checks if a text qualifies as a sentence. You do not need to modify this!
@@ -33,5 +24,27 @@ user_sentence = input("Enter a sentence: ")
 
 while (is_sentence(user_sentence) == False):
     print("This does not meet the criteria for a sentence.")
-    user_input = input("Enter a sentence: ")
+    user_sentence = input("Enter a sentence: ")
     
+cleaned = re.sub(r'[^\w\s]', '', user_sentence) #cleans out any punctuation or number
+split_sentence = cleaned.lower().split() #makes everything lowercase
+
+list_a = [] #for words
+list_b = [] #for frequncies
+
+for word in split_sentence:
+    #check if word has been found before
+    if word not in list_a:
+        list_a.append(word)
+        list_b.append(1)
+    
+    else:
+        #iterates over list_a for duplicates
+        index = list_a.index(word) #index() returns index 
+        list_b[index] += 1
+
+print(f'Input: "{user_sentence}"')
+print("Output:")
+#formats the output
+for i in range(len(list_a)):
+    print(f"{list_a[i]}: {list_b[i]}")

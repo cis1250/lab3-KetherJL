774721cbeff4278c3eece60413be30bf4b023379
KetherJL <jlim18@uoguelph.ca>
2025-10-20 12:18:34 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -20,31 +20,44 @@ def is_sentence(text):
 
     return True
 
-user_sentence = input("Enter a sentence: ")
-
-while (is_sentence(user_sentence) == False):
-    print("This does not meet the criteria for a sentence.")
+def get_valid_sentence():
     user_sentence = input("Enter a sentence: ")
-    
-cleaned = re.sub(r'[^\w\s]', '', user_sentence) #cleans out any punctuation or number
-split_sentence = cleaned.lower().split() #makes everything lowercase
+    while (is_sentence(user_sentence) == False):
+        print("This does not meet the criteria for a sentence.")
+        user_sentence = input("Enter a sentence: ")
+    return user_sentence
 
-list_a = [] #for words
-list_b = [] #for frequncies
+def clean_sentence(user_sentence):
+    cleaned = re.sub(r'[^\w\s]', '', user_sentence) #cleans out any punctuation or number
+    split_sentence = cleaned.lower().split() #makes everything lowercase
+    return split_sentence
 
-for word in split_sentence:
-    #check if word has been found before
-    if word not in list_a:
-        list_a.append(word)
-        list_b.append(1)
-    
-    else:
-        #iterates over list_a for duplicates
-        index = list_a.index(word) #index() returns index 
-        list_b[index] += 1
+def word_frequency(split_sentence):
 
-print(f'Input: "{user_sentence}"')
-print("Output:")
-#formats the output
-for i in range(len(list_a)):
-    print(f"{list_a[i]}: {list_b[i]}")
+    list_a = [] #for words
+    list_b = [] #for frequncies
+
+    for word in split_sentence:
+        #check if word has been found before
+        if word not in list_a:
+            list_a.append(word)
+            list_b.append(1)
+        
+        else:
+            #iterates over list_a for duplicates
+            index = list_a.index(word) #index() returns index 
+            list_b[index] += 1
+
+    return list_a, list_b
+
+def main():
+    user_sentence = get_valid_sentence()
+    split_sentence = clean_sentence(user_sentence)
+    list_a, list_b = word_frequency(split_sentence)
+    print(f'Input: "{user_sentence}"')
+    print("Output:")
+    for i in range(len(list_a)):
+        print(f"{list_a[i]}: {list_b[i]}")
+
+if __name__ == "__main__":
+    main()
